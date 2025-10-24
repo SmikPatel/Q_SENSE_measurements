65caf9591cb236d72e0714b3f9bf5a110f8e862d
Smik Patel <50188248+SmikPatel@users.noreply.github.com>
2025-10-24 01:08:13 -0400

    fix expectation/var to work for complex states

--- utils_results.py
+++ utils_results.py
@@ -2,17 +2,17 @@ import numpy as np
 from openfermion import get_sparse_operator
 
 def expectation(Op, State):
-    return (State @ Op @ State.T)[0,0]
+    return (State @ Op @ State.conj().T)[0,0]
 
 def matrix_element(Op, Bra, Ket):
-    return (Bra @ Op @ Ket.T)[0,0]
+    return (Bra @ Op @ Ket.conj().T)[0,0]
 
 def variance_of_operator(Op, State):
     """
     computes the variance of a Hermitian operator <psi|H^2|psi> - <psi|H|psi>^2
     """
-    first  = (State @ Op) @ (Op @ State.T)
-    second = (State @ Op @ State.T) ** 2
+    first  = (State @ Op) @ (Op @ State.conj().T)
+    second = (State @ Op @ State.conj().T) ** 2
     return first - second
 
 def variance_of_general_operator(Op, State):
@@ -22,9 +22,9 @@ def variance_of_general_operator(Op, State):
     note that qubit Hamiltonians with complex coefficients are not Hermitian. But a QWC or FC Hamiltonian can be measured independent of
     what the coefficients are
     """
-    first  = (State @ Op.conjugate().transpose()) @ (Op @ State.T)
-    second = (State @ Op @ State.T)
-    third  = (State @ Op.conjugate().transpose() @ State.T) 
+    first  = (State @ Op.conjugate().transpose()) @ (Op @ State.conj().T)
+    second = (State @ Op @ State.conj().T)
+    third  = (State @ Op.conjugate().transpose() @ State.conj().T) 
     return first - (second * third)
 
 def variance_of_decomp(decomp, State, N, general=False):
